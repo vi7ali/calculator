1adf8bfb5aba487e561a21bb17cc54cfefb2154c
Vitaly Osipov <vi7ali@gmail.com>
2018-12-04 16:26:35 +0300

    Created the basic form for the page with flexbox

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+	return a+b;
+}
+
+function subtract(a, b) {
+	return a-b;	
+}
+
+function multiply(a, b)  {
+	return a*b;
+}
+
+function divide(a, b) {
+	return a/b;
+}
+
+function operate(a, b, op) {
+	switch(op) {
+		case '+':
+			return add(a, b);
+			break;
+		case '-':
+			return subtract(a, b);
+			break;
+		case '*':
+			return multiply(a, b);
+			break;
+		case '/':
+			return divide(a, b);
+			break;
+		default:
+			console.log('Something went wrong in switch statement line 18');
+			break;
+	}
+}
