fba99a95fc8ca2f007f5fc0c2a3ca89d9b8f5287
Vitaly Osipov <vi7ali@gmail.com>
2018-12-14 21:05:05 +0300

    Implemented the '.' operator, added backspace button with functionality, implemented the floating point rounding function.

--- js/script.js
+++ js/script.js
@@ -1,87 +1,144 @@
-const buttons   = document.getElementById('buttons');
 const display   = document.getElementById('display');
 const formula   = document.getElementById('formula');
 const clear     = document.getElementById('clear');
 const equal     = document.getElementById('equals');
 const dot       = document.getElementById('dot');
 const negative  = document.getElementById('neg');
+const backspace = document.getElementById('backspace');
 const numbers   = Array.from(document.getElementsByClassName('number'));
 const operators = Array.from(document.getElementsByClassName('operator'));
+
+clear.addEventListener('click', clearInput, false);
+equal.addEventListener('click', equalInput, false);
+dot.addEventListener('click', dotInput, false);
+negative.addEventListener('click', negativeInput, false);
+backspace.addEventListener('click', backspaceInput, false);
+numbers.forEach(el=>el.addEventListener('click', numberInput, false));
+operators.forEach(el=>el.addEventListener('click', operatorInput, false));
 
 let inputValues = [];
 let userInput = '';
 
-dot.addEventListener('click', dotInput, false);
-negative.addEventListener('click', negativeInput, false);
-clear.addEventListener('click', clearInput, false);
-equal.addEventListener('click', equalInput, false);
-
-numbers.forEach(el=>el.addEventListener('click', numberInput, false));
-operators.forEach(el=>el.addEventListener('click', operatorInput, false));
+function backspaceInput() {
+	if(userInput.length) {
+		userInput = userInput.slice(0, -1);
+	}
+	showDisplay(userInput);
+}
 
 function dotInput() {
-	//TODO
+
+	if(!userInput.length) {
+		userInput = '0.';
+	}
+
+	if(!/\./.test(userInput)) {
+		userInput = userInput + '.';		
+	}
+	showDisplay(userInput);
 }
 
 function negativeInput() {
-	//TODO
+	if(userInput.length && userInput !== '0') {
+		if(!/^\-/.test(userInput)) {
+			userInput = '-' + userInput;
+		}
+		else {
+			userInput = userInput.slice(1);
+		}
+		showDisplay(userInput);
+	}
 }
 
 function clearInput() {
-	//TODO
+	inputValues = [];
+	userInput = '';
+	showDisplay('empty');
 }
 
 function numberInput(e) {
 	let input = e.target.textContent;
-		
-	userInput = userInput + input;	
-	
-	if(/^0[0-9]/.test(userInput)) {
+	userInput = userInput + input;
+
+	//Replacing the numbers that start with zero
+	if(/^0[0-9]/.test(userInput)) { 
 		userInput = input;
-	}
-	showDisplay(input);
+	}	
+	showDisplay(userInput);
 }
 
 function operatorInput(e) {
 	let input = e.target.textContent;
+	//Removing the decimal point if there are no numbers after it
+	if(userInput[userInput.length-1] === '.') {
+		userInput = userInput.slice(0, -1);
+	}
 
 	if(!userInput.length && !inputValues.length) {
 		showDisplay('empty');
 	}
-
-	if(/[/*-+]/.test(userInput)) {
-		userInput = input;
-	}
+	//Checking if the last input was an operator and replacing it with the new operator if it was
 	else {
+		if(!userInput) {						
+			inputValues.pop();
+			inputValues.push(input);
+		}
+		else {
+	//If it was a number then check for the floating point and round the number to 2 decimals if necessary
+		userInput = roundFloat(userInput);
 		inputValues.push(userInput);
 		inputValues.push(input);
-		showDisplay(input);
 		userInput = '';
+		}
+		showDisplay(input);
 	}
-
 }
 
 function equalInput() {
+	if(inputValues.length) {
+		if(!userInput) {
+			inputValues.pop();
+		}
+		else {
+			userInput = roundFloat(userInput);
+			inputValues.push(userInput);
+		}
 
+		let finalEquation = inputValues.join(' ');
+
+		let regexMD;
+		let regexAS;
+	}
 }
 
 function showDisplay(element) {
-	let lastElement = userInput.length-1;
-  if(element == 'empty') {
+  if(element === 'empty') {
   	display.textContent = '';
   	formula.textContent = '';
   }
-  else {
-  	if(Number.isNaN(parseFloat(element))) {
-  		formula.textContent = inputValues.join('');
-  		display.textContent = '';
-  	}
-		else {
-  		formula.textContent = inputValues.join('');
-  		display.textContent = display.textContent + element;
-  	}
-  }
+  if(Number.isNaN(parseFloat(element))) {
+ 		formula.textContent = inputValues.join(' ');
+ 		display.textContent = '';
+ 	}
+	else {
+ 		formula.textContent = inputValues.join(' ');
+ 		display.textContent = element;
+ 	}
 }
+
+function roundFloat(el) {
+	let number = parseFloat(el);
+	let regex = /\d+\.\d\d\d+/;
+	
+	if(regex.test(el)) {
+		number = Math.round(el*100)/100;
+		return number.toString();		
+	}
+	else {
+		return el;
+	}
+}
+
 
 function add(a, b) {
 	return a+b;
